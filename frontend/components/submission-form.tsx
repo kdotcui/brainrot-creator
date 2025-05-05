8441d4502653c38383041470bc49e710f5446a99
Kevin Cui <106274250+kdotcui@users.noreply.github.com>
2025-05-04 23:38:03 -0400

    added a prompt form for submission

---
+++ frontend/components/submission-form.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const formSchema = z.object({
+  prompt: z.string().min(10, {
+    message: "Prompt must be at least 10 characters long"
+  })
+})
+
+export function SubmissionForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+  
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="prompt"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Video Prompt To Be Created:</FormLabel>
+              <FormControl>
+                <Input placeholder="Narrate to your heart's content" {...field} />
+              </FormControl>
+              <FormDescription>
+                We suggest having a mix of video prompts
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Create your video!</Button>
+      </form>
+    </Form>
+  )
+
+}
